03188e863b8644d02bcb9305d6bdd4d4786b042e
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-10-19 18:02:04 -0400

    Setup account creation

--- src/components/Authenticator/components/CreateAccount/index.tsx
+++ src/components/Authenticator/components/CreateAccount/index.tsx
@@ -1,5 +1,5 @@
+import React, { useCallback, useState } from "react";
 import useAuth from "hooks/useAuth";
-import React from "react";
 
 const CreateAccount = ({
   onSwitchToSignIn,
@@ -9,9 +9,37 @@ const CreateAccount = ({
   const {
     actions: { createAccount },
   } = useAuth();
+  const [email, setEmail] = useState("");
+  const handleEmailChange = useCallback((e) => setEmail(e.target.value), []);
+  const [password, setPassword] = useState("");
+  const handlePasswordChange = useCallback(
+    (e) => setPassword(e.target.value),
+    []
+  );
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      createAccount({ email, password });
+    },
+    [createAccount, email, password]
+  );
   return (
     <div>
-      Creating account
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={handleEmailChange} />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <button type="submit">Create Account</button>
+        </label>
+      </form>
       <button onClick={onSwitchToSignIn}>Sign In</button>
     </div>
   );
